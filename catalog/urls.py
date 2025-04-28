cc02b8a83b5bb51fb5d02a31a11d442158569369
Vadim Burin <burinvad@mail.ru>
2025-04-29 00:06:27 +0300

    add views and patterns

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
+from catalog.views import home, contacts
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', ),
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
 ]
