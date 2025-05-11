668dc38db1321e5df3dfd69966fbe5365186c474
Vadim Burin <burinvad@mail.ru>
2025-05-11 23:52:08 +0300

    update project

--- catalog/models.py
+++ catalog/models.py
@@ -1,33 +1,34 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='категория', help_text='введите категорию')
+    description = models.TextField(verbose_name='описание категории', help_text='опишите категорию')
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=150, verbose_name='наименование продукта',
                             help_text='введите наименование продукта')
-    discription = models.TextField(verbose_name='описание продукта', help_text='опишите продукт')
+    description = models.TextField(verbose_name='описание продукта', help_text='опишите продукт')
     image = models.ImageField(upload_to='images/', blank=True, null=True, verbose_name='изображение',
                               help_text='загрузите изображение продукта')
-    category = models.CharField(max_length=150, verbose_name='категория', help_text='введите категорию продукта')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name='категория',
+                                 help_text='введите категорию продукта', null=True, blank=True, related_name='products')
     price = models.FloatField(default=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'{self.name}: {self.discription}'
 
     class Meta:
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
         ordering = ['category']
 
-
-class Category(models.Model):
-    name = models.CharField(max_length=150, verbose_name='категория', help_text='введите категорию')
-    discription = models.TextField(verbose_name='описание категории', help_text='опишите категорию')
-
     def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'категория'
-        verbose_name_plural = 'категории'
+        return f'{self.name}: {self.description}'
