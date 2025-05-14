0d431724c3d5fe3cc7a3e699d5fad2a87a432bd4
Vadim Burin <burinvad@mail.ru>
2025-05-15 00:21:04 +0300

    bugs fixes

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
 
-from catalog.views import product_list
+from catalog.views import product_list, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', product_list, name='product_list'),
+    path('products/<int:pk>/', product_detail, name='product_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
 
@@ -7,3 +7,10 @@ def product_list(request):
     products = Product.objects.all()
     context = {"products": products}
     return render(request, 'products_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'product_detail.html', context)
+    
