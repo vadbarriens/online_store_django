28816993ba3c2ef175b45fb8e1350b6896b0bcce
Vadim Burin <burinvad@mail.ru>
2025-05-16 22:28:28 +0300

    add migrate

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name='Заголовок')
     content = models.TextField(verbose_name='Содержимое')
@@ -7,7 +8,6 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     is_published = models.BooleanField(default=False, verbose_name='Опубликовано')
     views_count = models.PositiveIntegerField(default=0, verbose_name='Количество просмотров')
-
 
     def __str__(self):
         return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -7,9 +7,9 @@ from .views import (
 app_name = 'blog'
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post_list'),
-    path('create/', PostCreateView.as_view(), name='post_create'),
-    path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
-    path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+                path('', PostListView.as_view(), name='post_list'),
+                path('create/', PostCreateView.as_view(), name='post_create'),
+                path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+                path('<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
+                path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
