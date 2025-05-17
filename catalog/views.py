9bf226991b668cea967cfde89025f5dfb8471962
Vadim Burin <burinvad@mail.ru>
2025-05-17 20:28:16 +0300

    bugs fixes

--- catalog/views.py
+++ catalog/views.py
@@ -22,13 +22,13 @@ class ProductDetailView(DetailView):
     template_name = 'catalog/product_detail.html'
     context_object_name = 'products'
 
-class PostUpdateView(UpdateView):
+class ProductUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('catalog:product_list')
 
-class PostDeleteView(DeleteView):
+class ProductDeleteView(DeleteView):
     model = Product
     template_name = 'catalog/product_delete.html'
     success_url = reverse_lazy('catalog:product_list')
