aac300505d4947f9ce863695a9c861957d8c264a
Vadim Burin <burinvad@mail.ru>
2025-05-20 23:31:34 +0300

    add forms update requirements

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,10 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from catalog.apps import CatalogConfig
+from users.apps import UsersConfig
 
-from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
-
-app_name = CatalogConfig.name
+app_name = UsersConfig.name
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='product_list'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('products/create/', ProductCreateView.as_view(), name='product_create'),
-    path('products/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
-    path('products/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
+    path('login/', LoginView.as_view(template_name="login.html"), name='login'),
+    path('logout/', LogoutView.as_view(next_page='catalog:product_list'), name='logout'),
 ]
