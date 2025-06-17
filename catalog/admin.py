67c1c9b0c4ea315b54fc652fac884336e61ea1c1
Vadim Burin <burinvad@mail.ru>
2025-06-17 20:24:32 +0300

    update admin and product list

--- catalog/admin.py
+++ catalog/admin.py
@@ -6,10 +6,10 @@ from catalog.models import Product, Category
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name',)
-
+    search_fields = ('name', 'description')
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'price', 'category')
     list_filter = ('category',)
-    search_fields = ('name', 'description')
+    search_fields = ('name',)
