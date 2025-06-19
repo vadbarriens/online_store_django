1dce0b66e9af39fc96771f5dd57f8a672fa2d594
Vadim Burin <burinvad@mail.ru>
2025-06-19 20:51:22 +0300

    add services, update models and views

--- catalog/models.py
+++ catalog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 from users.models import User
+from django.template.defaultfilters import slugify
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название")
     description = models.TextField(verbose_name='описание категории', help_text='опишите категорию')
+    slug = models.SlugField(null=True)
 
     class Meta:
         verbose_name = 'категория'
@@ -14,6 +16,11 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
 
 class Product(models.Model):
--- config/settings.py
+++ config/settings.py
@@ -112,3 +112,12 @@ EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': os.getenv('LOCATION'),
+        }
+    }
